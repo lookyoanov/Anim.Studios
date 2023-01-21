822923db0179e3a768fb58348cb651445bce19af
lookyoanov <121449473+lookyoanov@users.noreply.github.com>
2023-01-21 12:23:51 +0300

    Add files via upload

---
+++ Anim.Studios/vendor/connect.php
@@ -0,0 +1,5 @@
+<?php
+
+     $connect = mysqli_connect(host: 'localhost', user: 'root', password: '', database: 'kliaksa_studio');
+
+?>
---
+++ Anim.Studios/vendor/registr.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once 'connect.php';
+
+$surname = $_POST['surname']
+$name = $_POST['name']
+$email = $_POST['email']
+$number = $_POST['number']
+
+?>
